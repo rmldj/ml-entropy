a115805e18765af34bb79630592d512d7ab92420
Romuald Janik <romuald.janik@gmail.com>
2019-09-16 21:27:25 +0200

    experiments.sh updated

--- code_paper/experiments.sh
+++ code_paper/experiments.sh
@@ -1,25 +1,29 @@
-python ising_entropy.py --log order --order natural
-python ising_entropy.py --log order --order maxcorr
-python ising_entropy.py --log order --order mincorr
-python ising_entropy.py --log order --order 0
-python ising_entropy.py --log order --order 137
-python ising_entropy.py --log order --order 555
-python ising_entropy.py --log order --order 1621
-python ising_entropy.py --log order --order 4567
+#python ising_entropy.py --log order --order natural
+#python ising_entropy.py --log order --order maxcorr
+#python ising_entropy.py --log order --order mincorr
+#python ising_entropy.py --log order --order 0
+#python ising_entropy.py --log order --order 137
+#python ising_entropy.py --log order --order 555
+#python ising_entropy.py --log order --order 1621
+#python ising_entropy.py --log order --order 4567
 
 
-python ising_entropy.py --log n -n 10000
-python ising_entropy.py --log n -n 20000
-python ising_entropy.py --log n -n 40000
+#python ising_entropy.py --log n -n 10000
+#python ising_entropy.py --log n -n 20000
+#python ising_entropy.py --log n -n 40000
 
 
-python ising_entropy.py --log cv -k 2
-python ising_entropy.py --log cv -k 3
-python ising_entropy.py --log cv -k 4
-python ising_entropy.py --log cv -k 5
+#python ising_entropy.py --order 0 --log cv -k 2
+#python ising_entropy.py --order 0 --log cv -k 3
+#python ising_entropy.py --order 0 --log cv -k 4
+#python ising_entropy.py --order 0 --log cv -k 5
 
 
-python ising_entropy.py --log hyper --depth 3 --n-estimators 100
-python ising_entropy.py --log hyper --depth 3 --n-estimators 200
-python ising_entropy.py --log hyper --depth 5 --n-estimators 100
-python ising_entropy.py --log hyper --depth 5 --n-estimators 200
+#python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 100
+#python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 200
+#python ising_entropy.py --order 0 --log hyper --depth 5 --n-estimators 100
+#python ising_entropy.py --order 0 --log hyper --depth 5 --n-estimators 200
+#python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 50
+#python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 25
+python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 75
+
