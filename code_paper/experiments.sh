4367055c2554e130fe0e4f6b4995f807dbcd6a0e
Romuald Janik <romuald.janik@gmail.com>
2019-09-18 18:12:27 +0200

    experiments.sh added more ordering experiments in other phases

--- code_paper/experiments.sh
+++ code_paper/experiments.sh
@@ -7,6 +7,23 @@
 #python ising_entropy.py --log order --order 1621
 #python ising_entropy.py --log order --order 4567
 
+python ising_entropy.py -T 1.5 --log order --order natural
+python ising_entropy.py -T 1.5 --log order --order maxcorr
+python ising_entropy.py -T 1.5 --log order --order mincorr
+python ising_entropy.py -T 1.5 --log order --order 0
+python ising_entropy.py -T 1.5 --log order --order 137
+python ising_entropy.py -T 1.5 --log order --order 555
+python ising_entropy.py -T 1.5 --log order --order 1621
+python ising_entropy.py -T 1.5 --log order --order 4567
+
+python ising_entropy.py -T 3.5 --log order --order natural
+python ising_entropy.py -T 3.5 --log order --order maxcorr
+python ising_entropy.py -T 3.5 --log order --order mincorr
+python ising_entropy.py -T 3.5 --log order --order 0
+python ising_entropy.py -T 3.5 --log order --order 137
+python ising_entropy.py -T 3.5 --log order --order 555
+python ising_entropy.py -T 3.5 --log order --order 1621
+python ising_entropy.py -T 3.5 --log order --order 4567
 
 #python ising_entropy.py --log n -n 10000
 #python ising_entropy.py --log n -n 20000
@@ -25,5 +42,5 @@
 #python ising_entropy.py --order 0 --log hyper --depth 5 --n-estimators 200
 #python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 50
 #python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 25
-python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 75
+#python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 75
 
