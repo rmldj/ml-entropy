1b7474cc4cd330dd5f72071e798c43f97c9a4c0a
Romuald Janik <romuald.janik@gmail.com>
2019-09-21 08:42:16 +0200

    pictures, linear extrapolation

--- code_paper/experiments.sh
+++ code_paper/experiments.sh
@@ -1,41 +1,3 @@
-#python ising_entropy.py --log order --order natural
-#python ising_entropy.py --log order --order maxcorr
-#python ising_entropy.py --log order --order mincorr
-#python ising_entropy.py --log order --order 0
-#python ising_entropy.py --log order --order 137
-#python ising_entropy.py --log order --order 555
-#python ising_entropy.py --log order --order 1621
-#python ising_entropy.py --log order --order 4567
-
-python ising_entropy.py -T 1.5 --log order --order natural
-python ising_entropy.py -T 1.5 --log order --order maxcorr
-python ising_entropy.py -T 1.5 --log order --order mincorr
-python ising_entropy.py -T 1.5 --log order --order 0
-python ising_entropy.py -T 1.5 --log order --order 137
-python ising_entropy.py -T 1.5 --log order --order 555
-python ising_entropy.py -T 1.5 --log order --order 1621
-python ising_entropy.py -T 1.5 --log order --order 4567
-
-python ising_entropy.py -T 3.5 --log order --order natural
-python ising_entropy.py -T 3.5 --log order --order maxcorr
-python ising_entropy.py -T 3.5 --log order --order mincorr
-python ising_entropy.py -T 3.5 --log order --order 0
-python ising_entropy.py -T 3.5 --log order --order 137
-python ising_entropy.py -T 3.5 --log order --order 555
-python ising_entropy.py -T 3.5 --log order --order 1621
-python ising_entropy.py -T 3.5 --log order --order 4567
-
-#python ising_entropy.py --log n -n 10000
-#python ising_entropy.py --log n -n 20000
-#python ising_entropy.py --log n -n 40000
-
-
-#python ising_entropy.py --order 0 --log cv -k 2
-#python ising_entropy.py --order 0 --log cv -k 3
-#python ising_entropy.py --order 0 --log cv -k 4
-#python ising_entropy.py --order 0 --log cv -k 5
-
-
 #python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 100
 #python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 200
 #python ising_entropy.py --order 0 --log hyper --depth 5 --n-estimators 100
@@ -43,4 +5,46 @@ python ising_entropy.py -T 3.5 --log order --order 4567
 #python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 50
 #python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 25
 #python ising_entropy.py --order 0 --log hyper --depth 3 --n-estimators 75
+
+python ising_entropy.py --order 0 --log n --n-estimators 50 -n 10000
+python ising_entropy.py --order 0 --log n --n-estimators 50 -n 20000
+python ising_entropy.py --order 0 --log n --n-estimators 50 -n 40000
+
+python ising_entropy.py --order 0 --log cv --n-estimators 50 -k 2
+python ising_entropy.py --order 0 --log cv --n-estimators 50 -k 3
+python ising_entropy.py --order 0 --log cv --n-estimators 50 -k 4
+python ising_entropy.py --order 0 --log cv --n-estimators 50 -k 5
+
+
+python ising_entropy.py --log order --n-estimators 50 --order natural
+python ising_entropy.py --log order --n-estimators 50 --order maxcorr
+python ising_entropy.py --log order --n-estimators 50 --order mincorr
+python ising_entropy.py --log order --n-estimators 50 --order 0
+python ising_entropy.py --log order --n-estimators 50 --order 137
+python ising_entropy.py --log order --n-estimators 50 --order 555
+python ising_entropy.py --log order --n-estimators 50 --order 1621
+python ising_entropy.py --log order --n-estimators 50 --order 4567
+
+python ising_entropy.py -T 1.5 --log order --n-estimators 50 --order natural
+python ising_entropy.py -T 1.5 --log order --n-estimators 50 --order maxcorr
+python ising_entropy.py -T 1.5 --log order --n-estimators 50 --order mincorr
+python ising_entropy.py -T 1.5 --log order --n-estimators 50 --order 0
+python ising_entropy.py -T 1.5 --log order --n-estimators 50 --order 137
+python ising_entropy.py -T 1.5 --log order --n-estimators 50 --order 555
+python ising_entropy.py -T 1.5 --log order --n-estimators 50 --order 1621
+python ising_entropy.py -T 1.5 --log order --n-estimators 50 --order 4567
+
+python ising_entropy.py -T 3.5 --log order --n-estimators 50 --order natural
+python ising_entropy.py -T 3.5 --log order --n-estimators 50 --order maxcorr
+python ising_entropy.py -T 3.5 --log order --n-estimators 50 --order mincorr
+python ising_entropy.py -T 3.5 --log order --n-estimators 50 --order 0
+python ising_entropy.py -T 3.5 --log order --n-estimators 50 --order 137
+python ising_entropy.py -T 3.5 --log order --n-estimators 50 --order 555
+python ising_entropy.py -T 3.5 --log order --n-estimators 50 --order 1621
+python ising_entropy.py -T 3.5 --log order --n-estimators 50 --order 4567
+
+
+
+
+
 
